bd52f148e168916e021d414a550b245fb4d7832d
Sarah Peters <sarahpieters15@gmail.com>
2020-06-01 10:04:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,5 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees, room_assignments)
-  puts "Hello, #{name}! You'll be assigned to room #{room}!"
+  puts "Hello, #{attendees}! You'll be assigned to room #{room_assignments}!"
 end
