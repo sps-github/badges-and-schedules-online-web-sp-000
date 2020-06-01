2f2cb40bb8f13f2689c3499c5945ba23791f1cb8
Sarah Peters <sarahpieters15@gmail.com>
2020-06-01 09:51:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,5 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 badge_maker(name)
 speakers.each do |name|
-puts "Hello, my name is #{name}"
+  puts "Hello, my name is #{name}"
 end
