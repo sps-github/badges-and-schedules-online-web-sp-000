8ce2b24495140676497842a745ba16f0fa80507a
Sarah Peters <sarahpieters15@gmail.com>
2020-06-01 09:59:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  attendees.each do |names|
-    puts "Hello, my name is #{attendees}"
+def batch_badge_creator(attendees)
+  attendees.each do |name|
+    badge_maker(name)
   end
 end
